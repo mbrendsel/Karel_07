55928cdcea41177256c4bacd6bceb4cd02d2d97b
mbrendsel <monica_brendsel@asl.org>
2014-12-06 15:12:18 +0000

    Update HillClimber.java

--- HillClimber.java
+++ HillClimber.java
@@ -8,25 +8,52 @@
 
 import edu.fcps.karel2.Display;
 
-public class HillClimber extends Climber {
 
-	public HillClimber(int x) {
-		//TODO Call the appropriate superclass constructor
-	}
-	
-	public void climbUpRight() {
-		//TODO Climb up one and across two
-	}
-	
-	public void climbUpLeft() {
-		//TODO Climb up one and across two
-	}
-	
-	public void climbDownRight() {
-		//TODO Climb down one and across two
-	}
-	
-	public void climbDownLeft() {
-		//TODO Climb down one and across two
-	}
+public class HillClimber extends Climber 
+{
+   public HillClimber()
+  {
+   super(8, 1, Display.NORTH, 1);
+  }
+   public HillClimber(int x, int y, int dir, int beep)
+  {
+   super(x, y, dir, beep);
+  }
+  
+   public void climbUpLeft()
+  {
+   turnRight();
+   move();
+   turnLeft();
+   move();
+   move();
+  }
+  
+   public void climbDownLeft()
+  {
+   move();
+   move();
+   turnLeft();
+   move();
+   turnRight();
+  }
+  
+   public void climbUpRight()
+  {
+   turnLeft();
+   move();
+   turnRight();
+   move();
+   move();
+  }
+  
+   public void climbDownRight()
+  {
+   move();
+   move();
+   turnRight();
+   move();
+   turnLeft();
+  }
+  
 }
