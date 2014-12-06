1df9036c1468b67c901f69463072459382cbb527
mbrendsel <monica_brendsel@asl.org>
2014-12-06 15:14:24 +0000

    Update StepClimber.java

--- StepClimber.java
+++ StepClimber.java
@@ -7,25 +7,47 @@
  */
 import edu.fcps.karel2.Display;
 
-public class StepClimber extends Climber {
-
-	public StepClimber(int x) {
-		//TODO Call the appropriate superclass constructor
-	}
-	
-	public void climbUpRight() {
-		//TODO Climb one step up and one across
-	}
-	
-	public void climbUpLeft() {
-		//TODO Climb one step up and one across
-	}
-	
-	public void climbDownRight() {
-		//TODO Climb one step down and one across
-	}
-	
-	public void climbDownLeft() {
-		//TODO Climb one step down and one across
-	}
+public class StepClimber extends Climber 
+{
+   public StepClimber()
+  {
+   super(8, 1, Display.NORTH, 1);
+  }
+   public StepClimber(int x, int y, int dir, int beep)
+  {
+   super(x, y, dir, beep);
+  }
+   
+   public void climbUpLeft()
+  {
+   turnRight();
+   move();
+   turnLeft();
+   move();
+  }
+  
+   public void climbDownLeft()
+  {
+   move();
+   turnLeft();
+   move();
+   turnRight();
+  }
+  
+   public void climbUpRight()
+  {
+   turnLeft();
+   move();
+   turnRight();
+   move();
+  }
+  
+   public void climbDownRight()
+  {
+   move();
+   turnRight();
+   move();
+   turnLeft();
+  }
+  
 }
